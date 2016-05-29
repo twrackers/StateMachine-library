496728e7f7fb039d9cd41b2058a1bc839fa02c26
Thomas W Rackers <twrackers@users.noreply.github.com>
2016-05-29 17:59:10 -0400

    Add files via upload

---
+++ sketches/BlinkOneLED/BlinkOneLED.ino
@@ -0,0 +1,21 @@
+#include <StateMachine.h>
+
+StateMachine blinker(1000, true);
+
+const int led = 9;
+bool state = false;   // false is OFF, true is ON
+
+void setup() 
+{
+  pinMode(led, OUTPUT);
+  digitalWrite(led, state ? HIGH : LOW);
+}
+
+void loop() 
+{
+  if (blinker.update()) {
+    state = !state;
+    digitalWrite(led, state ? HIGH : LOW);
+  }
+}
+
